0cf2cf970d4ac3ba1cc35dbc5f577d6b74372189
BlauskaerM <blauskaerm@protonmail.ch>
2020-11-09 23:08:39 +0100

    Able to encode vector of arbitrary length
    Current implementation has a bug however and does not encode correctly

--- src/main.rs
+++ src/main.rs
@@ -3,82 +3,144 @@ const BASE64_MAP: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01
 fn encode_base64_chunk(input_vec: &Vec<u8>) {
     let chunk_len = input_vec.len();
 
-    let (an, bn, cn, dn): (usize, usize, usize, usize);
-    let (a, b, c, d): (char, char, char, char);
+    println!("Chunk-len: {}", chunk_len);
 
-    match chunk_len {
-        1 => {
-            println!("Two padding");
-            an = ((input_vec[0] & 0xFC) >> 2) as usize;
-            bn = ((input_vec[0] & 0x03) << 4) as usize; //| ((input_vec[1] & 0xF0) >> 4)) as usize;
-                                                        //cn = (((input_vec[1] & 0x0F) << 2) | ((input_vec[1] & 0xC0) >> 6)) as usize;
-                                                        //dn = (input_vec[2] & 0x3F) as usize;
+    let (mut an, mut bn, mut cn, mut dn): (usize, usize, usize, usize);
+    let (mut a, mut b, mut c, mut d): (char, char, char, char);
 
-            a = BASE64_MAP.chars().nth(an).unwrap();
-            b = BASE64_MAP.chars().nth(bn).unwrap();
-            c = '=';
-            d = '=';
+    let mut n = 0;
+    loop {
+        //println!("n: {}", n);
+        if (n + 3) >= chunk_len {
+            break;
         }
-        2 => {
-            println!("One padding");
-            an = ((input_vec[0] & 0xFC) >> 2) as usize;
-            bn = (((input_vec[0] & 0x03) << 4) | ((input_vec[1] & 0xF0) >> 4)) as usize;
-            cn = ((input_vec[1] & 0x0F) << 2) as usize; //| ((input_vec[1] & 0xC0) >> 6)) as usize;
+
+        an = ((input_vec[n] & 0xFC) >> 2) as usize;
+        bn = (((input_vec[n] & 0x03) << 4) | ((input_vec[n + 1] & 0xF0) >> 4)) as usize;
+        cn = (((input_vec[n + 1] & 0x0F) << 2) | ((input_vec[n + 1] & 0xC0) >> 6)) as usize;
+        dn = (input_vec[n + 2] & 0x3F) as usize;
+
+        //println!("an: {}, bn: {}, cn: {}, dn: {}", an, bn, cn, dn);
+
+        a = BASE64_MAP.chars().nth(an).unwrap();
+        b = BASE64_MAP.chars().nth(bn).unwrap();
+        c = BASE64_MAP.chars().nth(cn).unwrap();
+        d = BASE64_MAP.chars().nth(dn).unwrap();
+
+        print!("{}{}{}{}", a, b, c, d);
+
+        n = n + 3;
+    }
+    let check_padding = chunk_len - n;
+    if check_padding == 1 {
+        //println!("Two padding");
+
+        an = ((input_vec[n] & 0xFC) >> 2) as usize;
+        bn = ((input_vec[n] & 0x03) << 4) as usize; //| ((input_vec[1] & 0xF0) >> 4)) as usize;
+                                                    //cn = (((input_vec[1] & 0x0F) << 2) | ((input_vec[1] & 0xC0) >> 6)) as usize;
+                                                    //dn = (input_vec[2] & 0x3F) as usize;
+
+        a = BASE64_MAP.chars().nth(an).unwrap();
+        b = BASE64_MAP.chars().nth(bn).unwrap();
+        c = '=';
+        d = '=';
+
+        print!("{}{}{}{}", a, b, c, d);
+    } else if check_padding == 2 {
+        //println!("One padding");
+
+        an = ((input_vec[n] & 0xFC) >> 2) as usize;
+        bn = (((input_vec[n] & 0x03) << 4) | ((input_vec[n + 1] & 0xF0) >> 4)) as usize;
+        cn = ((input_vec[n + 1] & 0x0F) << 2) as usize; //| ((input_vec[1] & 0xC0) >> 6)) as usize;
                                                         // dn = (input_vec[2] & 0x3F) as usize;
 
-            a = BASE64_MAP.chars().nth(an).unwrap();
-            b = BASE64_MAP.chars().nth(bn).unwrap();
-            c = BASE64_MAP.chars().nth(cn).unwrap();
-            d = '=';
-        }
-        _ => {
-            println!("No padding");
-            an = ((input_vec[0] & 0xFC) >> 2) as usize;
-            bn = (((input_vec[0] & 0x03) << 4) | ((input_vec[1] & 0xF0) >> 4)) as usize;
-            cn = (((input_vec[1] & 0x0F) << 2) | ((input_vec[1] & 0xC0) >> 6)) as usize;
-            dn = (input_vec[2] & 0x3F) as usize;
+        a = BASE64_MAP.chars().nth(an).unwrap();
+        b = BASE64_MAP.chars().nth(bn).unwrap();
+        c = BASE64_MAP.chars().nth(cn).unwrap();
+        d = '=';
 
-            //println!("an: {}, bn: {}, cn: {}, dn: {}", an, bn, cn, dn);
-
-            a = BASE64_MAP.chars().nth(an).unwrap();
-            b = BASE64_MAP.chars().nth(bn).unwrap();
-            c = BASE64_MAP.chars().nth(cn).unwrap();
-            d = BASE64_MAP.chars().nth(dn).unwrap();
-        }
+        print!("{}{}{}{}", a, b, c, d);
     }
+    // match chunk_len {
+    //     1 => {
+    //         println!("Two padding");
+    //         an = ((input_vec[0] & 0xFC) >> 2) as usize;
+    //         bn = ((input_vec[0] & 0x03) << 4) as usize; //| ((input_vec[1] & 0xF0) >> 4)) as usize;
+    //                                                     //cn = (((input_vec[1] & 0x0F) << 2) | ((input_vec[1] & 0xC0) >> 6)) as usize;
+    //                                                     //dn = (input_vec[2] & 0x3F) as usize;
 
-    print!("{}{}{}{}", a, b, c, d);
+    //         a = BASE64_MAP.chars().nth(an).unwrap();
+    //         b = BASE64_MAP.chars().nth(bn).unwrap();
+    //         c = '=';
+    //         d = '=';
+    //     }
+    //     2 => {
+    //         println!("One padding");
+    //         an = ((input_vec[0] & 0xFC) >> 2) as usize;
+    //         bn = (((input_vec[0] & 0x03) << 4) | ((input_vec[1] & 0xF0) >> 4)) as usize;
+    //         cn = ((input_vec[1] & 0x0F) << 2) as usize; //| ((input_vec[1] & 0xC0) >> 6)) as usize;
+    //                                                     // dn = (input_vec[2] & 0x3F) as usize;
+
+    //         a = BASE64_MAP.chars().nth(an).unwrap();
+    //         b = BASE64_MAP.chars().nth(bn).unwrap();
+    //         c = BASE64_MAP.chars().nth(cn).unwrap();
+    //         d = '=';
+    //     }
+    //     _ => {
+    //         println!("No padding");
+    //         an = ((input_vec[0] & 0xFC) >> 2) as usize;
+    //         bn = (((input_vec[0] & 0x03) << 4) | ((input_vec[1] & 0xF0) >> 4)) as usize;
+    //         cn = (((input_vec[1] & 0x0F) << 2) | ((input_vec[1] & 0xC0) >> 6)) as usize;
+    //         dn = (input_vec[2] & 0x3F) as usize;
+
+    //         //println!("an: {}, bn: {}, cn: {}, dn: {}", an, bn, cn, dn);
+
+    //         a = BASE64_MAP.chars().nth(an).unwrap();
+    //         b = BASE64_MAP.chars().nth(bn).unwrap();
+    //         c = BASE64_MAP.chars().nth(cn).unwrap();
+    //         d = BASE64_MAP.chars().nth(dn).unwrap();
+    //     }
+    // }
+
+    // print!("{}{}{}{}", a, b, c, d);
 }
 
 fn main() {
-    let mut input_vec: Vec<u8> = Vec::new();
+    //let mut input_vec: Vec<u8> = Vec::new();
     // input_vec.push('A' as u8);
     // input_vec.push('B' as u8);
     // input_vec.push('C' as u8);
 
-    // TWFu
-    input_vec.push('M' as u8);
-    input_vec.push('a' as u8);
-    input_vec.push('n' as u8);
-    print!("Base64: ");
-    encode_base64_chunk(&input_vec);
+    let test_string = String::from("any carnal pleasure.");
+    let test_string_len = test_string.len();
+    println!("Input string ({}): {}", test_string_len, test_string);
+    let test_byte_vec = test_string.into_bytes();
+    encode_base64_chunk(&test_byte_vec);
     println!();
-    input_vec.clear();
 
-    // TWE=
-    input_vec.push('M' as u8);
-    input_vec.push('a' as u8);
-    print!("Base64: ");
-    encode_base64_chunk(&input_vec);
-    println!();
-    input_vec.clear();
+    // // TWFu
+    // input_vec.push('M' as u8);
+    // input_vec.push('a' as u8);
+    // input_vec.push('n' as u8);
+    // print!("Base64: ");
+    // encode_base64_chunk(&input_vec);
+    // println!();
+    // input_vec.clear();
 
-    // TQ==
-    input_vec.push('M' as u8);
-    print!("Base64: ");
-    encode_base64_chunk(&input_vec);
-    println!();
-    input_vec.clear();
+    // // TWE=
+    // input_vec.push('M' as u8);
+    // input_vec.push('a' as u8);
+    // print!("Base64: ");
+    // encode_base64_chunk(&input_vec);
+    // println!();
+    // input_vec.clear();
+
+    // // TQ==
+    // input_vec.push('M' as u8);
+    // print!("Base64: ");
+    // encode_base64_chunk(&input_vec);
+    // println!();
+    // input_vec.clear();
 
     // println!(
     //     "Input: {}{}{}",
